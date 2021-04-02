fb6f7720514e7a67ea180d4e4840ccd1a49c3e55
Álvaro Peris <alvaroperis_06@hotmail.com>
2021-04-02 22:45:14 +0200

    update

--- habmapslib/setup.py
+++ habmapslib/setup.py
@@ -25,7 +25,6 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.9.1',
+        'Programming Language :: Python :: 3.9'
     ],
 ) 
