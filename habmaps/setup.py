0e431caa3b8a6312d162cc447866be63e2bbd4a3
Álvaro Peris <alvaroperis_06@hotmail.com>
2021-04-02 16:53:58 +0200

    update

--- habmaps/setup.py
+++ habmaps/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="habmaps",
-    version="0.0.1-snapshot0",
+    version="0.0.1-0",
     author="",
     author_email="",
     description="Librería para el acceso a habmaps",
