7af6b91bf52076a506141579f2ca8c31f9741ca2
Álvaro Peris <alvaroperis_06@hotmail.com>
2021-04-05 15:21:12 +0200

    update

--- demotraces/demoexec.sh
+++ demotraces/demoexec.sh
@@ -1,7 +1,18 @@
 export HABLIB_LOGLEVEL=INFO #INFO,ERROR
 export HABLIB_FORMAT="[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s"
-export HABLIB_FORMAT="p%(process)s -> %(levelname)s - %(message)s"
+export HABLIB_FORMAT="%(levelname)s - %(message)s" # <-- Para local, con menos verbose
 export HABLIB_LOGFILE="/tmp/hablibclient.log"
+
+PID=99999
+trap ctrl_c INT
+function ctrl_c() {
+  set -x
+  kill -9 $PID
+  exit 1
+  set +x
+}
+
 while [ true ]; do
     python3 -m habmapslib.cli --conffile config.yaml
+    PID=$($!)
 done
