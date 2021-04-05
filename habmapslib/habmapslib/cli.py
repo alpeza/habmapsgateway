b9ac77e154f4370d77c02681ec74caedd88c594a
Álvaro Peris <alvaroperis_06@hotmail.com>
2021-04-05 14:02:37 +0200

    update

--- habmapslib/habmapslib/cli.py
+++ habmapslib/habmapslib/cli.py
@@ -2,7 +2,7 @@ import click
 from . import confyaml
 from . import ConfHandler
 from . import Parser
-from . import initChecks
+from . import InitChecks
 import logging,os
 LOGLEVEL = os.environ.get('HABLIB_LOGLEVEL', 'INFO').upper()
 FORMATTER = os.environ.get('HABLIB_FORMAT', '[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s')
@@ -21,7 +21,7 @@ def cline(conffile, genconffile):
         ch = ConfHandler.ConfHandler(file=conffile)
         logging.info("Launching habmapslib parser with the next configuration:")
         logging.info(ch.getConfig())
-        initChecks.checkConfigs(ch)
+        InitChecks.checkConfigs(ch)
         p = Parser.Parser(ch)
         p.run()
 
