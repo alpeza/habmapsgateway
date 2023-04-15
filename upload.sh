76d9386f5fe98320043fe5645eef907b7ab80227
alpeza <80060968+alpeza@users.noreply.github.com>
2023-04-15 20:13:42 +0200

    update

--- upload.sh
+++ upload.sh
@@ -3,6 +3,6 @@ git commit -m "update"
 git push
 cp README.md habmapslib/README.md
 cd habmapslib
-python3 setup.py build sdist bdist_wheel
-python3 setup.py install
-python3 -m twine upload --skip-existing --verbose dist/*
+python3.9 setup.py build sdist bdist_wheel
+python3.9 setup.py install
+python3.9 -m twine upload --skip-existing --verbose dist/*
